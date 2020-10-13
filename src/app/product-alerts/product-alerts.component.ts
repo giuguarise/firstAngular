3ed13add3f28cc1ea76fda8e57269a8437e832f4
giuguarise <giu.guarise@gmail.com>
2020-10-13 09:59:33 -0300

    2 files modified, 3 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  @Input() product; //indica que valor vem do componente pai, product list component
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
